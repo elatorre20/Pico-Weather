92ebb121b0bbb8d34b8c9e457165c820578100ff
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2023-12-17 02:24:29 -0500

    Getting Data Correctly
    Setup basic functionality for reading weather data from weather.gov. Added ignored header for safe importing of SSID and password.

--- pico_weather/pico_weather.ino
+++ pico_weather/pico_weather.ino
@@ -1,9 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "secret.h"
 
+
+//wifi 
+char ssid[] = SSID_SECRET; //SSID and password are imported from ignored secret.h for security
+char pass[] = PASSWORD_SECRET;
+int status = WL_IDLE_STATUS; //status of Wifi connection
+char server[] = "forecast.weather.gov";  //National Weather Service Data
+WiFiClientSecure client; 
+
+
+void setup() {
+  Serial.begin(9600); //begin USB serial
+  status = WiFi.begin(ssid, pass); //attempt wifi connection
+  Serial.print("Wifi status: ");
+  if(status == WL_CONNECTED){ //if wifi connects successfully
+    client.setInsecure(); //connect to SSL server but ignore certs
+    Serial.println("Connected to WiFi!");
+    if(client.connect(server, 443)){ //for ssl requests
+      Serial.println("Connected to Website!");
+      client.println("GET https://forecast.weather.gov/product.php?site=BOX&issuedby=BOX&product=RWR&format=txt&version=1&glossary=0 HTTP/1.0"); //latest weather reports for boston region, updated hourly
+      client.println("Host:  forecast.weather.gov");
+      client.println("User-Agent: Pico_Weather/v0.1");
+      client.println("Connection: close");
+      client.println();
+      delay(1000);
+      int count = 0;
+      while(client.available()){ 
+        count++;
+        Serial.print((char)client.read());//print data from client to serial
+        // if(count == 64){//line break every 64 characters
+        //   count = 0;
+        //   Serial.println();
+        // }
+      }
+      Serial.println("Nothing more to read!");
+    }
+  }
+  else{
+    Serial.println("Connection Failed!");
+  }
 }
 
-void loop() {
+void loop() { //to know that the setup has finished runnign
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
